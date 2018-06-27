067ae686f6cbb0bb0bcdd9ade9099a0766f0ad91
Curtis Morice <curtismorice@gmail.com>
2018-06-27 17:46:11 -0500

    added pool and fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
+const pool = require('./modules/pool');
 const bodyParser = require('body-parser');
-
+const messages = require('./routes/messaging.router');
 // Route includes
 const colorRouter = require('./routes/color.router');
 
-// Body parser middleware
+//uses
+app.use(express.static('server/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 /* Routes */
 app.use('/api/colors', colorRouter);
-
-// Serve static files
 app.use(express.static('build'));
 
-// App Set //
-const PORT = process.env.PORT || 5000;
 
-/** Listen * */
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+//port
+const port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log(`listening on PORT`, port);
 });
+
+
+
+
+
+
+
+
